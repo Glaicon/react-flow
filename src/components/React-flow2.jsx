93bdd7155116ae69f949aad62110c2b0ce362568
Glaicon Reis <glaiconreis@gmail.com>
2021-07-02 18:17:54 +0100

    Adjusted react flow index

--- src/components/React-flow2.jsx
+++ src/components/React-flow2.jsx
@@ -18,11 +18,24 @@ const {TextArea} = Input;
 const initialElements = [
   {
     id: '0',
-    type: 'selectorNode',
+    type: 'input',
     data: {label: 'Start'},
     position: {x: 250, y: 5},
   },
 ];
+
+// const initialScenario = new Array({
+//   key: 1,
+//   description: 'Cenário 1',
+//   scenarios: [
+//     {
+//       key: '',
+//       source: '',
+//       target: '',
+//       value: '',
+//     },
+//   ],
+// });
 
 const DnDFlow = () => {
   const [textAreaVisible, setTextAreaVisible] = useState(false);
@@ -36,6 +49,7 @@ const DnDFlow = () => {
   const reactFlowWrapper = useRef(null);
   const [reactFlowInstance, setReactFlowInstance] = useState(null);
   const [scenarios, setScenarios] = useState(null);
+  const [allDictionaries, setDictionaries] = useState([]);
 
   const edgeTypes = {
     custom: CustomEdge,
@@ -146,6 +160,309 @@ const DnDFlow = () => {
     setEdgeInputVisible(false);
   };
 
+  const generateScenarios3 = () => {
+    setDictionaries([]);
+    var outputsList = elements.filter((e) => e.type === 'output');
+    var connections = elements.filter((el) => el.source !== undefined);
+    var elementsSelecteds = elements.filter((el) => el.source === undefined);
+
+    console.log(outputsList);
+    var maxKey = 1;
+    outputsList.forEach((out) => {
+      var connInitialOutput = connections.filter(
+        (conn) => conn.target === out.id
+      )[0];
+      var initialSource = out.id;
+      if (connInitialOutput.label) {
+        initialSource = (parseInt(out.id) + 1).toString();
+      }
+      var countConn = 0;
+      var key = maxKey++;
+      let scenarioInitial = {
+        key: `${out.id}-${countConn}`,
+        source: out.id,
+        target: out.id,
+        value: out.data.label,
+      };
+      let scenario = {
+        key: key,
+        description: `Cenário ${key}`,
+        scenarios: [
+          {
+            key: scenarioInitial.key,
+            source: initialSource,
+            target: scenarioInitial.target,
+            value: scenarioInitial.value,
+          },
+        ],
+      };
+      console.log(scenario);
+      console.log(scenarioInitial);
+      var scenarioLast = scenarioInitial;
+      do {
+        var connectionSelected = connections.filter(
+          (conn) => conn.target === scenarioLast.source
+        )[0];
+        // connections.forEach((conn) =>
+        // if (conn.target === s.source) {
+        console.log(connectionSelected);
+        console.log(connections);
+
+        var elementSelected = elementsSelecteds.filter(
+          (c) => c.id === connectionSelected.source
+        )[0];
+        console.log(elementSelected);
+        console.log(elementsSelecteds);
+        if (connectionSelected.label) {
+          scenarioLast = {
+            key: `${connectionSelected.source}-${connectionSelected.target}-${
+              countConn + 1
+            }`,
+            source: connectionSelected.source,
+            target: connectionSelected.target,
+            value: elementSelected.data.label,
+          };
+          scenario.scenarios.push(scenarioLast);
+          scenarioLast = {
+            key: `${connectionSelected.source}-${connectionSelected.target}-${countConn}`,
+            source: connectionSelected.source,
+            target: connectionSelected.target,
+            value: connectionSelected.label,
+          };
+          scenario.scenarios.push(scenarioLast);
+        } else {
+          scenarioLast = {
+            key: `${connectionSelected.source}-${connectionSelected.target}-${countConn}`,
+            source: connectionSelected.source,
+            target: connectionSelected.target,
+            value: elementSelected.data.label,
+          };
+          if (scenarioLast.source !== '0') {
+            scenario.scenarios.push(scenarioLast);
+          }
+          countConn++;
+        }
+      } while (scenarioLast.source !== '0');
+      scenario.scenarios.sort((a, b) => a.source - b.source);
+      allDictionaries.push(scenario);
+      // scenario.scenarios.forEach((s) => {
+      //   console.log(s)
+      //   var connectionSelected = connections.filter(
+      //     (conn) => conn.target === s.target
+      //   );
+      //   // connections.forEach((conn) =>
+      //   // if (conn.target === s.source) {
+      //     console.log(connectionSelected)
+      //     // console.log(connections)
+
+      //   var elementSelected = elementsSelecteds.filter(
+      //     (c) => c.id === connectionSelected.target
+      //   );
+      //   console.log(elementSelected)
+      //   console.log(elementsSelecteds)
+      //   if (connectionSelected.label) {
+      //     var scen = {
+      //       key: `${connectionSelected.source}-${connectionSelected.target}-${countConn}`,
+      //       source: connectionSelected.source,
+      //       target: connectionSelected.target,
+      //       value: connectionSelected.label,
+      //     };
+      //   } else {
+      //     scen = {
+      //       key: `${connectionSelected.source}-${connectionSelected.target}-${countConn}`,
+      //       source: connectionSelected.source,
+      //       target: connectionSelected.target,
+      //       value: elementSelected.data.label,
+      //     };
+      //   }
+      //   scenario.scenarios.push(scen);
+      //   // }
+      //   // });
+      //   countConn++;
+      // });
+
+      console.log(scenario.scenarios);
+      console.log(allDictionaries);
+      // allDictionaries.push(scenario);
+      setDictionaries(allDictionaries);
+    });
+    // });
+    var listValues = [];
+    allDictionaries.forEach((element) => {
+      listValues.push(
+        <div key={element.key + '-dic'}>
+          <h2>{element.description}</h2>
+          {element.scenarios.map((sce) => {
+            return (
+              <p id={sce.key} style={{marginLeft: '1%'}}>
+                {sce.value}
+              </p>
+            );
+          })}
+        </div>
+      );
+    });
+    setScenarios(listValues);
+  };
+
+  const generateScenarios2 = () => {
+    console.log(allDictionaries);
+    elements.sort((a, b) => a.source - b.source);
+    var connections = elements.filter((el) => el.source !== undefined);
+    var elementsSelecteds = elements.filter(
+      (el) => el.source === undefined && el.id !== 0
+    );
+    var countConn = 0;
+    connections.forEach((conn) => {
+      var elementSelected = elementsSelecteds.filter(
+        (c) => c.id === conn.target
+      )[0];
+      var connection = allDictionaries.filter((c) =>
+        c.scenarios.filter((sce) => sce.source === conn.source)
+      );
+      let keys = allDictionaries.map((a) => a.key);
+      var maxKey = Math.max(...keys);
+      if (connection) {
+        setDictionaries((allDictionaries) =>
+          allDictionaries.map((el) => {
+            console.log('Connection');
+            console.log(connection);
+            console.log(el.scenarios);
+            var scenarios = el.scenarios.filter(
+              (scen) => scen.source !== connection.scenarios[0].source
+            );
+            if (conn.label) {
+              el.key = maxKey++;
+              el.description = `Cenário ${el.key}`;
+              el.scenarios = {
+                ...scenarios,
+                key: `${conn.source}-${conn.target}-${countConn}`,
+                source: conn.source,
+                target: conn.target,
+                value: conn.label,
+              };
+            } else {
+              el.key = maxKey++;
+              el.description = `Cenário ${el.key}`;
+              el.scenarios = {
+                ...scenarios,
+                key: `${conn.source}-${conn.target}-${countConn}`,
+                source: conn.source,
+                target: conn.target,
+                value: elementSelected.data.label,
+              };
+            }
+            return el;
+          })
+        );
+        // } else {
+        //   console.log("ENTROU AQUI")
+        //   setDictionaries((dicts) =>
+        //     dicts.dictionaries.map((el) => {
+        //       var dictSelected = el.scenarios.filter(
+        //         (sc) => sc.source === conn.source
+        //       )[0];
+        //       if (conn.label) {
+        //         if (dictSelected) {
+        //           el.key = dictSelected.key;
+        //           el.description = dictSelected.description;
+        //           el.scenarios = {
+        //             ...el.scenarios,
+        //             key: `${conn.source}-${conn.target}-${countConn}`,
+        //             source: conn.source,
+        //             target: conn.target,
+        //             value: conn.label,
+        //           };
+        //         }
+        //       } else {
+        //         if (dictSelected) {
+        //           el.key = dictSelected.key;
+        //           el.description = dictSelected.description;
+        //           el.scenarios = {
+        //             ...el.scenarios,
+        //             key: `${conn.source}-${conn.target}-${countConn}`,
+        //             source: conn.source,
+        //             target: conn.target,
+        //             value: elementSelected.data.label,
+        //           };
+        //         }
+        //       }
+        //       return dicts;
+        //     })
+        //   );
+      }
+    });
+    //   }
+    //     var maxKey = Math.max(...allDictionaries.dictionaries.keys);
+    //    allDictionaries.dictionaries.push({
+    //     key: maxKey++,
+    //     description: `Cenário ${key}`,
+    //     scenarios: [
+    //       ...scenarios,{
+    //         key: `${conn.source}-${conn.target}-${countConn}`,
+    //         source: conn.source,
+    //         target: conn.target,
+    //         value: elementSelected.data.label,
+    //       },
+    //     ]
+    //    })
+    //    setDictionaries(allDictionaries);
+    //   } else {
+    //     var elementSelected = elementsSelecteds.filter(
+    //       (el) => el.id === conn.target
+    //     )[0];
+    //     console.log(elementSelected);
+    //     if (conn.label) {
+    //       var key = "";
+    //       if (
+    //         dictScenarios.filter(
+    //           (a) => a.key === `${conn.source}-${conn.target}-${countConn}`
+    //         )
+    //       ) {
+    //         key = `${conn.source}-${conn.target}-${countConn + 1}`;
+    //       } else {
+    //         key = `${conn.source}-${conn.target}-${countConn}`;
+    //       }
+    //       dictScenarios.push({
+    //         key: key,
+    //         source: conn.source,
+    //         target: conn.target,
+    //         value: conn.label,
+    //       });
+    //     }
+    //     dictScenarios.push({
+    //       key: `${conn.source}-${conn.target}-${countConn}`,
+    //       source: conn.source,
+    //       target: conn.target,
+    //       value: elementSelected.data.label,
+    //       lastNode: elementSelected.type === "output",
+    //     });
+    //   }
+    // });
+    var listValues = [];
+    allDictionaries.forEach((element) => {
+      listValues.push(
+        <>
+          <h2>{element.description}</h2>
+          {element.scenarios.forEach((sce) => {
+            return (
+              <p id={sce.key} style={{marginLeft: '1%'}}>
+                {sce.value}
+              </p>
+            );
+          })}
+          ;
+        </>
+      );
+    });
+    setScenarios(listValues);
+
+    console.log(allDictionaries);
+    console.log(connections);
+    console.log(elements);
+  };
+
+  const createDictionary = () => {};
   const generateScenarios = () => {
     var dictScenarios = []; // create an empty array
     elements.sort((a, b) => a.source - b.source);
@@ -166,9 +483,9 @@ const DnDFlow = () => {
                   (a) => a.key === `${c.source}-${c.target}-${countConn}`
                 )
               ) {
-                key = `${c.source}-${c.target}-${countConn + 1}`;
-              } else {
                 key = `${c.source}-${c.target}-${countConn}`;
+              } else {
+                key = `${c.source}-${c.target}-${countConn + 1}`;
               }
               dictScenarios.push({
                 key: key,
@@ -222,18 +539,18 @@ const DnDFlow = () => {
       y: event.clientY - reactFlowBounds.top,
     });
     var ids = [];
-
+    console.log(elements);
     elements.forEach((el) => {
       if (el.id && el.source === undefined) {
         console.log(el.id);
         ids.push(parseInt(el.id));
       }
     });
+    console.log(ids);
     var lastId = Math.max(...ids);
     console.log(lastId);
-    console.log(lastId++);
     const newNode = {
-      id: `${lastId++}`,
+      id: `${lastId + 1}`,
       type,
       position,
       data: {label: `${type} node`},
@@ -361,10 +678,10 @@ const DnDFlow = () => {
           </div>
         </ReactFlowProvider>
         <div className="scenarios__controls">
-          <Button style={{margin: '10px'}} onClick={() => generateScenarios()}>
+          <Button style={{margin: '10px'}} onClick={() => generateScenarios3()}>
             Gerar Cenários
           </Button>
-          <di>{scenarios}</di>
+          {scenarios}
         </div>
       </div>
     </div>
